b599f6146621a0c1e3bba1449d400ddd4a83eb62
Aman Birdi <amanbirdi777@gmail.com>
2018-11-21 00:46:13 +0530

    actions added for redux

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+
+import {createStore} from 'redux';
+import {Providers} from 'react-redux';
+
+let store = createStore();
+
+const App = () => {
+    <Providers store={store}>
+        
+    </Providers>
+}
+
 AppRegistry.registerComponent(appName, () => App);
